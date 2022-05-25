9afda96c5589005ab0184ad0006ec0d6bb2b708e
Hritik Singh <47533908+hritik7501@users.noreply.github.com>
2022-05-25 12:59:55 +0530

    Add files via upload

---
+++ day5-random/que1.sh
@@ -0,0 +1,6 @@
+#Use Random Function (( RANDOM )) to get Single Digit
+
+#!/bin/bash -x
+
+
+echo $((RANDOM%10))
---
+++ day5-random/que2.sh
@@ -0,0 +1,6 @@
+#Use Random to get Dice Number between 1 to 6
+
+
+#!/bin/bash -x
+
+echo $((RANDOM%6+1))
